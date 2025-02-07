ef9aefca12b6051f7cf942a161100cae44bb815b
atpopov21 <atpopov21@codingburgas.bg>
2025-02-08 00:44:56 +0200

    Finish Main page (Close #13); Add page templates

--- Main-Thread/Main-Thread.PL/MainPage.xaml.cs
+++ Main-Thread/Main-Thread.PL/MainPage.xaml.cs
@@ -1,25 +1,15 @@
-﻿using System.Windows.Input;
+﻿using Microsoft.Maui.Graphics.Platform;
+using System.Reflection;
+using System.Windows.Input;
 
 namespace Main_Thread.PL
 {
     public partial class MainPage : ContentPage
     {
-        // Links to the developers' social media profiles and application repository
-        public ICommand ClickedDevOne => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-        public ICommand ClickedDevTwo => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-        public ICommand ClickedGitHubRepository => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-        public ICommand ClickedDiscussAndSupport => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-        public ICommand ClickedSecurityPolicy => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
-
-            BoxView[] leftStripes = { StripeOneL, StripeTwoL, StripeThreeL, StripeFourL, StripeFiveL, StripeSixL, StripeSevenL, StripeEigthL, StripeNineL, StripeTenL, StripeElevenL };
-            BoxView[] rightStripes = { StripeOneR, StripeTwoR, StripeThreeR, StripeFourR, StripeFiveR, StripeSixR, StripeSevenR, StripeEigthR, StripeNineR, StripeTenR, StripeElevenR };
-            StripesDetailsGenerator(leftStripes);
-            StripesDetailsGenerator(rightStripes);
+            MainThreadLogo.BackgroundColor = Color.Parse("Transparent");
         }
         protected override void OnAppearing()
         {
@@ -28,17 +18,6 @@ namespace Main_Thread.PL
             this.Window.MinimumWidth = 1000;
         }
 
-        private void StripesDetailsGenerator(BoxView[] stripes)
-        {
-            foreach (BoxView stripe in stripes)
-            {
-                Random randomValue = new Random();
-                int randomLength = randomValue.Next(50, 380);
-                Color randomColor = Color.FromRgb(randomValue.Next(150, 235), randomValue.Next(140, 235), randomValue.Next(150, 235));
-                stripe.WidthRequest = randomLength;
-                stripe.BackgroundColor = randomColor;
-            }
-        }
     }
 
 }
