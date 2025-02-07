65d55d77b8a4aa14f0e9978741506fe526b35a71
atpopov21 <atpopov21@codingburgas.bg>
2025-02-07 22:01:54 +0200

    Add basic Main page layout (Close #9); Add icon to application

--- Main-Thread/Main-Thread.PL/MainPage.xaml.cs
+++ Main-Thread/Main-Thread.PL/MainPage.xaml.cs
@@ -2,23 +2,32 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+
+            BoxView[] leftStripes = { StripeOneL, StripeTwoL, StripeThreeL, StripeFourL, StripeFiveL, StripeSixL, StripeSevenL, StripeEigthL, StripeNineL, StripeTenL, StripeElevenL };
+            BoxView[] rightStripes = { StripeOneR, StripeTwoR, StripeThreeR, StripeFourR, StripeFiveR, StripeSixR, StripeSevenR, StripeEigthR, StripeNineR, StripeTenR, StripeElevenR };
+            StripesDetailsGenerator(leftStripes);
+            StripesDetailsGenerator(rightStripes);
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.Window.MinimumHeight = 600;
+            this.Window.MinimumWidth = 1000;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void StripesDetailsGenerator(BoxView[] stripes)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            foreach (BoxView stripe in stripes)
+            {
+                Random randomValue = new Random();
+                int randomLength = randomValue.Next(50, 380);
+                Color randomColor = Color.FromRgb(randomValue.Next(150, 235), randomValue.Next(140, 235), randomValue.Next(150, 235));
+                stripe.WidthRequest = randomLength;
+                stripe.BackgroundColor = randomColor;
+            }
         }
     }
 
