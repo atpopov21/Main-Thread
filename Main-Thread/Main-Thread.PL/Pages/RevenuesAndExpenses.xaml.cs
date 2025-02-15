fd58a3269114a36154862b49112af5586b47d396
atpopov21 <atpopov21@codingburgas.bg>
2025-02-15 22:40:32 +0200

    Add Home page layout (Close #24); Add dynamic charts (Implemented Microcharts + SkiaSharp); Updated BGLT;

---
+++ Main-Thread/Main-Thread.PL/Pages/RevenuesAndExpenses.xaml.cs
@@ -0,0 +1,9 @@
+namespace Main_Thread.PL.Pages;
+
+public partial class RevenuesAndExpenses : ContentPage
+{
+	public RevenuesAndExpenses()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Main-Thread/Main-Thread.PL/Pages/StockManagement.xaml.cs
@@ -0,0 +1,9 @@
+namespace Main_Thread.PL.Pages;
+
+public partial class StockManagement : ContentPage
+{
+	public StockManagement()
+	{
+		InitializeComponent();
+	}
+}
